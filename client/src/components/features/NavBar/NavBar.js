e0e36efa1132aacdbf61035ea80505704a8c5965
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2019-08-11 08:30:14 +0200

    Revert "Change navbar"
    This reverts commit c380b1ededdc16eb7199f064a08e33957eb11ce0.

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,25 +1,27 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 import Logo from '../../common/Logo/Logo';
 import MainMenu from '../../layout/MainMenu/MainMenu';
 import './NavBar.scss';
 
 class NavBar extends React.Component {
-  state = {
-    links: [
+
+  render() {
+    const links = [
       { path: '/', title: 'Home' },
       { path: '/posts', title: 'Posts' },
       { path: '/posts/new', title: 'Add post' },
       { path: '/contact', title: 'Contact' }
-    ],
-  }
-
-  render() {
-    const { links } = this.state;
+    ];
     return (
-      <nav className="navbar">
+      <div className="nav-bar">
+        {/*// <Link to='/'>Home</Link>
+        // <Link to='/posts'>Posts</Link>
+        // <Link to='/posts/new'>Add post</Link>
+        // <Link to='/contact'>Contact</Link>*/}
         <Logo />
         <MainMenu links={links} />
-      </nav>
+      </div>
     );
   }
 }

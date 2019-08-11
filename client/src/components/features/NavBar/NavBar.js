c380b1ededdc16eb7199f064a08e33957eb11ce0
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2019-08-11 08:11:32 +0200

    Change navbar

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,27 +1,25 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import Logo from '../../common/Logo/Logo';
 import MainMenu from '../../layout/MainMenu/MainMenu';
 import './NavBar.scss';
 
 class NavBar extends React.Component {
-
-  render() {
-    const links = [
+  state = {
+    links: [
       { path: '/', title: 'Home' },
       { path: '/posts', title: 'Posts' },
       { path: '/posts/new', title: 'Add post' },
       { path: '/contact', title: 'Contact' }
-    ];
+    ],
+  }
+
+  render() {
+    const { links } = this.state;
     return (
-      <div className="nav-bar">
-        {/*// <Link to='/'>Home</Link>
-        // <Link to='/posts'>Posts</Link>
-        // <Link to='/posts/new'>Add post</Link>
-        // <Link to='/contact'>Contact</Link>*/}
+      <nav className="navbar">
         <Logo />
         <MainMenu links={links} />
-      </div>
+      </nav>
     );
   }
 }

d41c387d85fe719e82b945d384ef6b7aa5847a47
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2019-08-14 22:19:12 +0200

    remove DevTools, deploy on Heroku

---
+++ client/build/precache-manifest.b8c519cbe7e761ae990ef9d38eb7f12b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad2c08e398f948d610a4553c263c87c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2507faa031086b055c0c",
+    "url": "/static/css/2.d109cac1.chunk.css"
+  },
+  {
+    "revision": "1f8938d1ac6f9967eb79",
+    "url": "/static/css/main.98e9830a.chunk.css"
+  },
+  {
+    "revision": "2507faa031086b055c0c",
+    "url": "/static/js/2.f0171cd8.chunk.js"
+  },
+  {
+    "revision": "1f8938d1ac6f9967eb79",
+    "url": "/static/js/main.428bb5a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000/api';
-export const BASE_URL = 'http://localhost:3000';
+export const API_URL = 'https://damp-river-81166.herokuapp.com/api';
+export const BASE_URL = 'https://damp-river-81166.herokuapp.com/';
